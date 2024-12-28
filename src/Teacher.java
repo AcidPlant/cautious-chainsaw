464d2b09464c1e821c182bd9efde5a0ba301d239
AcidPlant <acidplayer200775@gmail.com>
2024-12-28 10:39:47 +0500

    s

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,12 +1,11 @@
 import java.util.*;
-public class Teacher {
-    private Person person;
+public class Teacher extends Person {
     private String subject;
     private int yearsOfExperience;
     private int salary;
 
     public Teacher(String name, String surname, int age, boolean gender, String subject, int yearsOfExperience, int salary) {
-        this.person = new Person(name, surname, age, gender);
+        super(name, surname, age, gender);
         this.subject = subject;
         this.yearsOfExperience = yearsOfExperience;
         this.salary = salary;
@@ -14,12 +13,12 @@ public class Teacher {
 
     public void giveRaise(double percentage) {
         if (percentage > 0) {
-            this.salary += (this.salary * percentage / 100);
+            this.salary += ((this.salary * percentage) / 100);
         }
     }
 
     @Override
     public String toString() {
-        return person.toString() + " I teach " + subject + ".";
+        return super.toString() + " I teach " + subject + ".";
     }
 }
